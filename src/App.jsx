f2993cbb6cc14caab1b024006c9a50ddcaf5adab
Ali Zulfikar <62384197+alizul01@users.noreply.github.com>
2022-12-24 20:22:27 +0700

    Deep dive fetch and others

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,36 @@
-import ReactDOM from "react-dom";
-import Pet from "./Pet";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import Details from "./Details";
+import SearchParams from "./SearchParams";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      cacheTime: Infinity,
+    },
+  },
+});
 
 const App = () => {
-  <div>
-    <h1>Adopt Me!</h1>
-    <Pet name="Luna" breed="Havanese" animal="dog" />
-    <Pet name="Pepper" breed="Cockatiel" animal="bird" />
-    <Pet name="Doink" breed="Mixed" animal="cat" />
-  </div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+          <header>
+            <Link to="/">Adopt Me!</Link>
+          </header>
+          <Routes>
+            <Route path="/details/:id" element={<Details />} />
+            <Route path="/" element={<SearchParams />} />
+          </Routes>
+        </QueryClientProvider>
+      </BrowserRouter>
+    </div>
+  );
 };
+
 const container = document.getElementById("root");
-const root = ReactDOM.createRoot(container);
+const root = createRoot(container);
 root.render(<App />);
